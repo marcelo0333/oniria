e5cc7b607c032cc3ee1a64f4172591a6572caa9e
marcelo0333 <139006301+marcelo0333@users.noreply.github.com>
2026-01-10 17:55:33 -0300

    feat: create bg starlfield with canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { Starfield } from "@/public/components/Starfield";
+import { useState } from "react";
 
 export default function Home() {
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-b from-[#05010D]/80 via-[#0B0620]/60 to-black">
+        <Starfield />
+      <main className="flex min-h-screen w-full z-10 max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+        <h1 className="font-[var(--font-playfair)] text-5xl  text-zinc-900 dark:text-zinc-100 sm:text-6xl">
+          Welcome to Oniria
+        </h1>
+        <button className="mt-6 rounded bg-[#7B5CFA] px-4 py-2 font-semibold text-zinc-100 hover:bg-[#6A4FF1] focus:outline-none focus:ring-2 focus:ring-[#7B5CFA] focus:ring-offset-2 dark:ring-offset-black">
+          Let&apos;s Dream together
+        </button>
+        <p className="mt-4 text-zinc-600 dark:text-zinc-400 sm:text-lg">
+          Your journey into the world of dreams starts here, explore and create!
+        </p>
       </main>
     </div>
   );
---
+++ public/components/Starfield.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+
+
+
+export function Starfield() {
+    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+    const numStars = 500;
+
+    useEffect(() => {
+      const canvas = canvasRef.current!
+      const ctx = canvas.getContext("2d")!
+
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+
+      const stars = Array.from({ length: numStars }, () => ({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 1.5,
+        speed: Math.random() * 0.5 + 0.2,
+        depth : Math.random() * 1.0,
+        blur: Math.random() * 2.0,
+      }))
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        stars.forEach(star => {
+            star.x -= star.speed
+            if (star.x < 0) {
+                star.x = canvas.width
+                star.y = Math.random() * canvas.height
+            }
+            ctx.beginPath()
+            ctx.arc(star.x, star.y, star.radius * star.depth * star.blur, 0, Math.PI * 2)
+            ctx.fillStyle = "white"
+            ctx.fill()
+        })
+        requestAnimationFrame(animate)
+    }
+    animate()
+    }, [])
+    return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-full z-0" />;
+}
